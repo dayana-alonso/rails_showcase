6040ef588b464477fcfee06e791796e6f119c6a7
Dayana Alonso <dayanalon83@gmail.com>
2019-12-03 17:01:39 +0000

    adding _shared nav to layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -6,6 +6,19 @@ class VisitorsController < ApplicationController
 		@visitor = Visitor.new
 	end
 
-	# def create
-	# end
+	def create
+		@visitor = Visitor.new(strong_params)
+		if @visitor.save
+			redirect_to new_visitor_path
+		end
+	end
+
+	def edit
+		@visitor = Visitor.find(params[:id])
+	end
+
+	def strong_params
+		params.require(:visitor).permit(:name, :visitor_cardnumber)
+	end
+
 end
